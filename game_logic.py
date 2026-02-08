e6c1277a1d9000406db44b00c2c9855f9e089248
Kenan999 <55302817+Kenan999@users.noreply.github.com>
2026-02-08 17:59:39 +0100

    Step 3: Refactor code and enhance game logic

---
+++ game_logic.py
@@ -0,0 +1,57 @@
+import random
+from ascii_art import STAGES
+
+WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+def get_random_word():
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+def play_game():
+    secret_word = get_random_word()
+    guessed_letters = []
+    mistakes = 0
+    max_mistakes = len(STAGES) - 1
+
+    print("Welcome to Snowman Meltdown!")
+
+    while mistakes < max_mistakes:
+        display_game_state(mistakes, secret_word, guessed_letters)
+        
+        guess = input("Guess a letter: ").lower()
+        
+        if guess in guessed_letters:
+            print("You already guessed that letter!")
+            continue
+            
+        guessed_letters.append(guess)
+        
+        if guess in secret_word:
+            print("Good guess!")
+            
+            all_guessed = True
+            for letter in secret_word:
+                if letter not in guessed_letters:
+                    all_guessed = False
+                    break
+            
+            if all_guessed:
+                display_game_state(mistakes, secret_word, guessed_letters)
+                print("Congratulations! You saved the snowman!")
+                return
+        else:
+            mistakes += 1
+            print("Wrong guess!")
+
+    display_game_state(mistakes, secret_word, guessed_letters)
+    print("Oh no! The snowman has melted. The word was: " + secret_word)
